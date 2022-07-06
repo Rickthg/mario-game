f3887dcef6596f3c97eb8d47bc25ef2e4aa64c52
Thiago Guimaraes <thiagoguimaraesaragao@gmail.com>
2022-07-06 00:27:38 -0300

    criei colisão com o casco da tartaruga

--- js/script.js
+++ js/script.js
@@ -14,13 +14,25 @@ setTimeout(() =>{
 }
 
 const loop = setInterval(() => {
+    
+    const mario = document.querySelector('.mario')
 
     const turtle = document.querySelector('.turtle')
 
     const turtlePosition = turtle.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '  ');
 
-    if (turtlePosition <= 120){
+    console.log (marioPosition);
+    if (turtlePosition <= 95 && turtlePosition > 0 && marioPosition < 80) {
+
         turtle.style.animation = 'none';
+        turtle.style.left = `${turtlePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png';
+
     }
 }, 10);
 
